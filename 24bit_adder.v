c5b19f8acdbfc9f925395660f3f4de3d16ed11dc
ALBIN SHAJU <118779469+Albin-shaju@users.noreply.github.com>
2023-07-27 23:34:49 +0530

    Update 24bit_adder.v

--- 24bit_adder.v
+++ 24bit_adder.v
@@ -1,10 +1,34 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2023 11:02:12 PM
+// Design Name: 
+// Module Name: adder_24
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-module 24bit_adder(
-  input [23:0] a,b,
-  output [23:0] sum
-  output carry);
 
-{carry,sum} = a + b;
+module Adder_24Bit(
+    input [23:0] A,
+    input [23:0] B,
+    output reg [23:0] sum,
+    output reg carry_out
+);
+
+    always @* begin
+        {carry_out, sum} = A + B;
+    end
 
 endmodule
